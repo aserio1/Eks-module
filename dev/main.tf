f1cdc0fde9d9a9d816988d0746ca617ec6d96a0c
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-02-25 14:16:15 -0600

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,1 +1,12 @@
+module "eks" {
+  source = "../../modules/eks"
 
+  name               = "${local.name_prefix}-eks"
+  region             = var.region
+  vpc_cidr           = var.vpc_cidr
+  az_count           = var.az_count
+  kubernetes_version = var.kubernetes_version
+
+  node_group = var.node_group
+  tags       = local.common_tags
+}
