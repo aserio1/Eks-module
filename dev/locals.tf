eac9b403d8ad75f35ee4224e58f8edfccf75f099
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-02-25 14:15:16 -0600

    Update locals.tf

--- dev/locals.tf
+++ dev/locals.tf
@@ -1,1 +1,10 @@
+locals {
+  env         = "dev"
+  name_prefix = "${var.project}-${local.env}"
 
+  common_tags = {
+    Project     = var.project
+    Environment = local.env
+    ManagedBy   = "Terraform"
+  }
+}
