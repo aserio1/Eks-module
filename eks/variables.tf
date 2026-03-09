46fb777152d83f5c1350fbaee08b53e9db57ba79
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-09 16:12:16 -0500

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -974,3 +974,46 @@ variable "alb_access_log_audit_bucket" {
   type        = string
 }
 
+##############Provider 
+
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+
+      }
+    }
+    backend "s3" {
+        encrypt = "true"
+        bucket  = "iadalfa-tfstate"
+        region = "us-gov-west-1"
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+    default_tags {
+        tags = {
+            Name        = "${var.project_name}-provider"
+        }
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+    alias = "ecs-role"
+    assume_role {
+      role_arn = "arn:aws-us-gov:iam::262763737219:role/ALFA-Deploy-Role"
+    }
+    default_tags {
+        tags = {
+            Name        = "${var.project_name}-provider"
+        }
+    }
+}
+
+locals {
+    branchid = (var.branch == "main" ? "main" : "${var.branch}")
+    lower_id = lower("${var.project_name}-${local.branchid}")
+}
