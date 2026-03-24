cb2b9afd401411b6a917a2805e3b9b22f6db2fad
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-24 14:47:19 -0500

    Update output.tf

--- eks/output.tf
+++ eks/output.tf
@@ -1,27 +1,39 @@
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.this.name
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.this.arn
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.this.endpoint
+  description = "EKS endpoint"
+  value       = aws_eks_cluster.this.endpoint
 }
 
-output "cluster_ca" {
-  value = aws_eks_cluster.this.certificate_authority[0].data
-}
-
-output "vpc_id" {
-  value = aws_vpc.this.id
-}
-
-output "private_subnet_ids" {
-  value = values(aws_subnet.private)[*].id
-}
-
-output "public_subnet_ids" {
-  value = values(aws_subnet.public)[*].id
+output "cluster_version" {
+  description = "EKS version"
+  value       = aws_eks_cluster.this.version
 }
 
 output "node_group_name" {
-  value = aws_eks_node_group.managed.node_group_name
+  description = "Managed node group name"
+  value       = aws_eks_node_group.managed.node_group_name
+}
+
+output "alb_dns_name" {
+  description = "ALB DNS name"
+  value       = aws_lb.eks.dns_name
+}
+
+output "alb_arn" {
+  description = "ALB ARN"
+  value       = aws_lb.eks.arn
+}
+
+output "sns_topic_arn" {
+  description = "SNS topic ARN"
+  value       = aws_sns_topic.eks_alerts.arn
 }
