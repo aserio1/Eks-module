57465461f14281dad4d807182ef7f27ae9b81b1b
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-24 14:56:10 -0500

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,37 +1,104 @@
-variable "project" {
+variable "project_name" {
+  description = "Project name"
   type        = string
-  description = "Project name used for naming"
 }
 
-variable "region" {
+variable "branch" {
+  description = "Git branch name"
   type        = string
-  description = "AWS GovCloud region (us-gov-west-1 or us-gov-east-1)"
 }
 
-variable "vpc_cidr" {
+variable "aws_region" {
+  description = "AWS region"
   type        = string
-  description = "VPC CIDR"
 }
 
-variable "kubernetes_version" {
+variable "aws_account_id" {
+  description = "AWS account ID"
   type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Public subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet IDs"
+  type        = list(string)
+}
+
+variable "iam_role" {
+  description = "IAM role ARN"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "ACM certificate ARN"
+  type        = string
+}
+
+variable "eks_version" {
   description = "EKS version"
-  default     = "1.29"
+  type        = string
 }
 
-variable "az_count" {
+variable "node_group_name" {
+  description = "Managed node group name"
+  type        = string
+}
+
+variable "node_instance_types" {
+  description = "Node instance types"
+  type        = list(string)
+}
+
+variable "node_desired_size" {
+  description = "Desired node size"
   type        = number
-  description = "AZ count"
-  default     = 2
 }
 
-variable "node_group" {
-  type = object({
-    instance_types = list(string)
-    capacity_type  = string
-    min_size       = number
-    max_size       = number
-    desired_size   = number
-    disk_size      = number
-  })
+variable "node_min_size" {
+  description = "Minimum node size"
+  type        = number
+}
+
+variable "node_max_size" {
+  description = "Maximum node size"
+  type        = number
+}
+
+variable "alb_ingress_rules" {
+  description = "Ingress rules for ALB"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "alb_egress_rules" {
+  description = "Egress rules for ALB"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "alert_email" {
+  description = "SNS alert email"
+  type        = string
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
 }
