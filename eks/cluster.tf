2e7622a0d33ce954f807bbc03a8a5ded27f28b84
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-24 14:46:39 -0500

    Update and rename main.tf to cluster.tf

---
+++ eks/cluster.tf
@@ -0,0 +1,128 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  provider = aws.eks-role
+  name     = "${local.cluster_name}-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  provider   = aws.eks-role
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws-us-gov:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  provider = aws.eks-role
+  name     = "${local.cluster_name}-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  provider   = aws.eks-role
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws-us-gov:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  provider   = aws.eks-role
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws-us-gov:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  provider   = aws.eks-role
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws-us-gov:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = local.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.eks_version
+
+  vpc_config {
+    subnet_ids              = var.private_subnets
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    security_group_ids      = [aws_security_group.eks_nodes.id]
+  }
+
+  enabled_cluster_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_cloudwatch_log_group.eks
+  ]
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_node_group" "managed" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.private_subnets
+
+  instance_types = var.node_instance_types
+  capacity_type  = "ON_DEMAND"
+
+  scaling_config {
+    desired_size = var.node_desired_size
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_readonly
+  ]
+
+  tags = merge(local.common_tags, {
+    Name = var.node_group_name
+  })
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "kube-proxy"
+
+  depends_on = [
+    aws_eks_node_group.managed
+  ]
+
+  tags = local.common_tags
+}
