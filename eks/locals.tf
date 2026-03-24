a8c013accdd6cca0012c80c53b3a9a0f270af6b9
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-24 14:37:20 -0500

    Create locals.tf

---
+++ eks/locals.tf
@@ -0,0 +1,18 @@
+data "aws_region" "current" {}
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+
+locals {
+  eks_alb_log_bucket_name = "${var.project_name}-alb-access-log-${data.aws_region.current.name}"
+  cluster_name            = "${var.project_name}-eks"
+
+  common_tags = merge(
+    {
+      Name        = local.cluster_name
+      Project     = var.project_name
+      Application = var.project_name
+      ManagedBy   = "Terraform"
+    },
+    var.tags
+  )
+}
