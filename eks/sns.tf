ff3d5253a3b7983a96a460990933a1e823a61202
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-03-25 14:07:57 -0500

    Update sns.tf

--- eks/sns.tf
+++ eks/sns.tf
@@ -1,11 +1,14 @@
 resource "aws_sns_topic" "eks_alerts" {
-  name = "${local.cluster_name}-alerts"
+  count = var.create_sns_topic ? 1 : 0
 
+  name = "${local.cluster_name}-alerts"
   tags = local.common_tags
 }
 
 resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.eks_alerts.arn
+  count = var.create_sns_topic ? 1 : 0
+
+  topic_arn = aws_sns_topic.eks_alerts[0].arn
   protocol  = "email"
   endpoint  = var.alert_email
 }
