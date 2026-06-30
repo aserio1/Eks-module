05d50ac4d6a9dc9c8f4b3b08f4290199da6d4884
Aserio1 <64231793+aserio1@users.noreply.github.com>
2026-06-30 13:55:53 -0500

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,2 +1,24 @@
-terraform init -no-color -backend-config region=us-east-1 -backend-config bucket=imac-prod-757265181315-tf-state-hhsoig -backend-config dynamodb_table=imac-prod-757265181315-tf-lock-hhsoig --backend-config key=AWSReservedSSO_hhs-oig-cloud-analytics-tier2_ee08d5cc50a1f525/Scott.Hutchison@oig.hhs.gov -upgrade -reconfigure
-13:25:10  Initializing the backend...
+aws cloudtrail lookup-events \
+  --lookup-attributes AttributeKey=EventName,AttributeValue=CreateEnvironmentEC2 \
+  --profile 757265181315 \
+  --query 'Events[].Username'
+
+
+
+aws ec2 describe-instances \
+  --profile 757265181315 \
+  --filters "Name=tag-key,Values=aws:cloud9:environment" \
+  --query 'Reservations[].Instances[].{
+      InstanceId:InstanceId,
+      Name:Tags[?Key==`Name`]|[0].Value,
+      Cloud9Env:Tags[?Key==`aws:cloud9:environment`]|[0].Value,
+      State:State.Name,
+      PrivateIP:PrivateIpAddress
+  }' \
+  --output table
+
+
+
+aws ec2 describe-instances \
+  --profile 757265181315 \
+  --filters "Name=tag:aws:cloud9:environment,Values=<environment-id>"
